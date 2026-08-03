66cb08cbf561ad4c5d679d727c211ea9c2c3e636
Gabriel Ricci <gr.ricci.gabriel@gmail.com>
2026-08-03 12:38:53 -0300

    migration to vercel deploy config

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 # exit on error
 set -o errexit
 
-pip install -r requirements.txt
-python manage.py collectstatic --no-input
+python3 -m pip install -r requirements.txt
+python3 manage.py collectstatic --noinput
